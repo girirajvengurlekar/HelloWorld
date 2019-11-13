d8768bf6c74871c0650b90baf40b01d4bd518e0c
girirajvengurlekar <girirajvengurlekar@users.noreply.github.com>
2019-11-13 15:42:47 +0530

    Create CopyFile.java

---
+++ src/oata/CopyFile.java
@@ -0,0 +1,39 @@
+package oata;
+
+import java.io.*;
+ 
+/**
+ * This program demonstrates how to copy a file using the
+ * FileInputStream and FileOutputStream classes with a
+ * byte array as a buffer.
+ * @author Giriraj
+ */
+public class CopyFile {
+    private static final int BUFFER_SIZE = 4096;
+ 
+    public static void main(String[] args) {
+        if (args.length < 2) {
+            System.out.println("Please provide input and output files");
+            System.exit(0);
+        }
+ 
+        String inputFile = args[0];
+        String outputFile = args[1];
+ 
+ 
+        try (
+            InputStream inputStream = new FileInputStream(inputFile);
+            OutputStream outputStream = new FileOutputStream(outputFile);
+        ) {
+ 
+            byte[] buffer = new byte[BUFFER_SIZE];
+            int bytesRead = -1;
+ 
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
